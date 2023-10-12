76366c2b995199d8dd97e2c9303da7f592941058
Toby Chen <chenyanjun912@hotmail.com>
2023-10-11 23:42:04 -0700

    Redo wireguard.sh

--- wireguard.sh
+++ wireguard.sh
@@ -1,66 +1,70 @@
 #!/bin/bash
 
-# Ensure the script is run as root
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root" 
-    exit 1
-fi
+# Update and upgrade system
+sudo apt update && sudo apt upgrade -y
 
-# Get Ubuntu version
-UBUNTU_VERSION=$(lsb_release -rs)
+# Install wireguard
+sudo apt install -y wireguard-tools
 
-# Update the system and install software-properties-common
-apt update && apt upgrade -y
-apt install software-properties-common -y
+# Generate private and public key for wireguard
+PRIVATE_KEY=$(wg genkey)
+PUBLIC_KEY=$(echo "$PRIVATE_KEY" | wg pubkey)
 
-# Add WireGuard repository for Ubuntu 18.04
-if [[ $UBUNTU_VERSION == "18.04" ]]; then
-    add-apt-repository ppa:wireguard/wireguard -y
-    apt update
-    apt install wireguard-dkms wireguard-tools linux-headers-$(uname -r) iptables-persistent -y
-else
-    apt install wireguard-tools iptables-persistent -y
-fi
+# Generate client keys
+CLIENT_PRIVATE_KEY=$(wg genkey)
+CLIENT_PUBLIC_KEY=$(echo "$CLIENT_PRIVATE_KEY" | wg pubkey)
 
-ufw allow 51820/udp
+# Generate random ULA prefix
+# ULA prefix is in the range fd00::/8 to fdff::/8
+# Generate random hexadecimal values for the subsequent 40 bits
+ULA_PREFIX=$(printf "fd%x:%x:%x" "$(($RANDOM/256))" "$RANDOM" "$RANDOM")
 
-# Configure WireGuard
-umask 077
-cd /etc/wireguard
-wg genkey | tee privatekey | wg pubkey > publickey
+# Detect default gateway interface name
+MAIN_INTERFACE=$(ip route | grep default | awk '{print $5}' | head -n 1)
 
-# Prompt user for client's public key
-read -p "Enter the client's public key: " CLIENT_PUBLIC_KEY
+# Enable IP forwarding
+sudo sh -c 'echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf'
+sudo sh -c 'echo "net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.conf'
+sudo sysctl -p
 
-# Generate server configuration
-cat > wg0.conf <<EOL
+# Allow WireGuard port in UFW
+sudo ufw allow 16383/udp
+
+# Write the wg0.conf file
+cat > /etc/wireguard/wg0.conf <<EOF
 [Interface]
-Address = 10.0.0.1/24
-SaveConfig = true
-PrivateKey = $(cat privatekey)
-ListenPort = 51820
+PrivateKey = $PRIVATE_KEY
+Address = 10.8.0.1/24, ${ULA_PREFIX}::1/64
+ListenPort = 16383
+PostUp = ufw route allow in on wg0 out on ${MAIN_INTERFACE}
+PostUp = iptables -t nat -I POSTROUTING -o ${MAIN_INTERFACE} -j MASQUERADE
+PostUp = ip6tables -t nat -I POSTROUTING -o ${MAIN_INTERFACE} -j MASQUERADE
+PreDown = ufw route delete allow in on wg0 out on ${MAIN_INTERFACE}
+PreDown = iptables -t nat -D POSTROUTING -o ${MAIN_INTERFACE} -j MASQUERADE
+PreDown = ip6tables -t nat -D POSTROUTING -o ${MAIN_INTERFACE} -j MASQUERADE
 
 [Peer]
 PublicKey = $CLIENT_PUBLIC_KEY
-AllowedIPs = 10.0.0.2/32
-
-EOL
+AllowedIPs = 10.8.0.2/32, ${ULA_PREFIX}::2/64
+EOF
 
 # Start and enable WireGuard
 systemctl start wg-quick@wg0
 systemctl enable wg-quick@wg0
 
-# Enable IP Forwarding
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sysctl -p
+# Echo client config
+echo "Client Configuration:"
+echo "----------------------"
+cat <<EOF
 
-# Set up NAT with iptables
-iptables -A FORWARD -i wg0 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o wg0 -j ACCEPT
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+[Interface]
+PrivateKey = $CLIENT_PRIVATE_KEY
+Address = 10.8.0.2/24, ${ULA_PREFIX}::2/64
+DNS = 10.8.0.1
 
-# Save iptables rules to be persistent across reboots
-iptables-save > /etc/iptables/rules.v4
+[Peer]
+PublicKey = $PUBLIC_KEY
+Endpoint = [Your-Server-IP]:16383
+AllowedIPs = 0.0.0.0/0, ::/0
 
-echo "WireGuard is now set up with forwarding to the internet enabled!"
-echo "Client with IP 10.0.0.2 has been added."
+EOF
